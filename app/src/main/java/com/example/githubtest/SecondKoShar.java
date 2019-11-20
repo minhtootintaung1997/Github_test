c1ffef220ba06feb3ebac70bcdf0eb9acbfe20c6
kks <kaungkhantshar.devmm@gmail.com>
2019-11-21 05:20:25 +0630

    koshar activity

---
+++ app/src/main/java/com/example/githubtest/SecondKoShar.java
@@ -0,0 +1,14 @@
+package com.example.githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondKoShar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_ko_shar);
+    }
+}
