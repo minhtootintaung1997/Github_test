3ba7196c9248fc9341036e4fedf2ed9ba9efaa2f
Min Htoo Tint Aung <minhtootintaungdev.mm@gmail.com>
2019-11-21 05:21:32 +0630

    created thirdmmin activity

---
+++ app/src/main/java/com/example/githubtest/ThirdMin.java
@@ -0,0 +1,14 @@
+package com.example.githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdMin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_min);
+    }
+}
